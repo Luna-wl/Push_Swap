12724abe9f5291627ee600e10eb7dd01a08a66b3
Warintorn Luedara <wluedara@z3t2c3.42bangkok.com>
2023-01-07 19:27:36 +0700

    quick sort

---
+++ quick_sort.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quick_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wluedara <Warintorn_L@outlook.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 12:49:42 by wluedara          #+#    #+#             */
+/*   Updated: 2023/01/07 17:06:08 by wluedara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	*list_to_arr(t_stack **stack, int len)
+{
+	int		*arr;
+	int		i;
+	t_stack	*tmp;
+
+	arr = malloc(sizeof(int) * len);
+	if (!arr)
+		return (0);
+	i = 0;
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		arr[i++] = tmp->num;
+		tmp = tmp->next;
+	}
+	return (arr);
+}
+
+void	swap(int *a, int *b)
+{
+	int	t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int	find_pi_position(int *arr, int max, int min)
+{
+	int	pivot;
+	int	i;
+	int	k;
+	
+	pivot = arr[max];
+	i = min - 1;
+	k = min;
+	while (k < max)
+	{
+		if (arr[k] <= pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[k]);
+		}
+		k++;
+	}
+	swap(&arr[i + 1], &arr[max]);
+	return (i + 1);
+}
+
+void	quick_sort_sage(int *arr, int max, int min)
+{
+	int	pivot;
+
+	if (min < max)
+	{
+		pivot = find_pi_position(arr, max, min);
+		quick_sort_sage(arr, pivot - 1, min);
+		quick_sort_sage(arr, max, pivot + 1);
+	}
+}
+
+void	init_index2list(t_stack **stack)
+{
+	int		len;
+	int		*arr;
+	int		i;
+	t_stack	*new;
+
+	len = stack_lenght(stack);
+	arr = list_to_arr(stack, len);
+	quick_sort_sage(arr, len - 1, 0);
+	new = *stack;
+	// print_arr(arr);
+	while (new != NULL)
+	{
+		i = 0;
+		while (i < len)
+		{
+			if (arr[i] == new->num)
+				new->index = i + 1;
+			i++;
+		}
+		new = new->next;
+	}
+}
