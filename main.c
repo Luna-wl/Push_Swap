71e4bdf73a18a42a9561905c62b2efa10fb33d91
Warintorn Luedara <sky@Warintorns-MacBook-Air.local>
2023-01-10 17:31:52 +0700

    add sort process

---
+++ main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wluedara <wluedara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 16:35:55 by wluedara          #+#    #+#             */
+/*   Updated: 2023/01/10 17:28:28 by wluedara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int ac, char **av)
+{
+	int		i;
+	t_stack	*a_stack;
+	t_stack	*b_stack;
+
+	i = 1;
+	a_stack = NULL;
+	b_stack = NULL;
+	if (ac >= 2)
+	{
+		
+		while (i < ac)
+		{
+			put_in_stack(av[i], &a_stack);
+			i++;
+		}
+		if (dup_mai(&a_stack) == 0)
+			error406(&a_stack, 0);
+		if (truat_stack(&a_stack) == 1)
+			error406(&a_stack, 1);
+		sort_sage(&a_stack, &b_stack);
+		printf("=== After ===\n");
+		printf("a = \n");
+		my_print(a_stack);
+		lop_stack(&a_stack);
+	}
+	return (0);
+}
